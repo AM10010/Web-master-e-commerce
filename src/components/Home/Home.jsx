aa6f71d2766c153d02e8c40274691283d9f9f190
AM10010 <mabanoub826@gmail.com>
2025-04-07 20:40:08 +0200

    Update some responsive styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,24 +10,32 @@ import Bestselling from "./BestSelling/BestSelling";
 import Adbanner from "./Adbanner/Adbanner";
 import Explore from "./Explore/Explore";
 import NewArrival from "./Newarrival/NewArrival";
+
 const HomePage = () => {
   return (
     <div className="container my-5">
-      <div className="home-hero  d-flex align-items-center justify-content-around">
-        <div className="mt-4">
+      <div className="home-hero row align-items-start justify-content-between">
+        {/* Sidebar on left */}
+        <div className="col-12 col-lg-3 mb-4 mb-lg-0">
           <Sidebar />
         </div>
-        <div>
+
+        {/* Hero banner on right */}
+        <div className="col-12 col-lg-9">
           <Hero />
         </div>
       </div>
+
       <hr />
+
       {/* Flash Sales Section */}
       <FlashSale products={[]} />
       <hr className="mt-5" />
+
       {/* Browse by Category */}
       <Catogery />
       <hr className="mt-5" />
+
       {/* Best Selling Products */}
       <Bestselling products={[]} />
 
@@ -37,9 +45,10 @@ const HomePage = () => {
       {/* Explore Our Products */}
       <Explore />
 
-
       {/* New Arrivals */}
       <NewArrival />
+
+      {/* Services */}
       <Service />
     </div>
   );
